182b8abc6b2eff5db457f3db0e4871e00a6e2272
LUIS GUSTAVO SIMIONI <130366635+LuisGustavoSimioni@users.noreply.github.com>
2024-10-04 15:14:10 +0000

    Fiz as rotas, porém n fiz a home

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const app = express();
 const PORT = 3300;
 
 app.get("/", function (req, res) {
-  res.send(__dirname);
+  res.sendFile(__dirname + "/src/views/index.html");
+  //res.sendFile(__dirname + "/src/views/about.html");
 });
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+
+//ROTA ULTILIZANDO MIDDLEWARE(404)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
+});
+
 app.listen(PORT, () => {
   console.log("running...");
 });
